5909a874732131b7eb852388ccf8a9f9fd607a38
Tristan Leo <mail@tristan-leo.de>
2011-12-24 12:06:49 +0100

    added cache-control headers, beautified code a bit

--- status.js
+++ status.js
@@ -11,20 +11,27 @@ http.createServer(function (req, res) {
     'Access-Control-Allow-Origin' : '*',
     'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
     'Access-Control-Allow-Headers': '*',
+    'Cache-Control'               : 'no-cache, no-store, must-revalidate',
     'Content-Type'                : 'application/javascript'
   });
+
   if (req.method === "GET") {
     var params = url.parse(req.url, true).query;
+
     if (!_.isUndefined(params.toggle) && (params.toggle === "fnord")) {
       lastPoll = new Date();
+    } else {
+      respondStatus(res);
     }
-    respondStatus(res);
   }
 }).listen(4242, "0.0.0.0");
 console.log('Server running at http://127.0.0.1:4242/');
 
 function respondStatus (res) {
-  var minutesSinceLastPoll = _.isUndefined(lastPoll) ? closeAfterMinutes+1 : lastPoll.getMinutesBetween(new Date());
+  var minutesSinceLastPoll = _.isUndefined(lastPoll) ?
+                                closeAfterMinutes+1 :
+                                lastPoll.getMinutesBetween(new Date());
+
   if(minutesSinceLastPoll > closeAfterMinutes) {
     res.end("false");
   } else {
