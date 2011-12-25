c18fbbbf6606e73373aa147e6c08beccd723d400
Tristan Leo <mail@tristan-leo.de>
2011-12-25 13:48:29 +0100

    added tweet feature

--- status.js
+++ status.js
@@ -1,40 +1,86 @@
 var http = require('http'),
-    url = require('url'),
-    _ = require('underscore');
-require('date-utils');
+  url = require('url'),
+  _ = require('underscore'),
+  twitter = require('ntwitter'),
+  date_utils = require('date-utils');
 
-var lastPoll;
+var lastPoll = null;
+var lastState = false;
 var closeAfterMinutes = 5;
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {
-    'Access-Control-Allow-Origin' : '*',
-    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
-    'Access-Control-Allow-Headers': '*',
-    'Cache-Control'               : 'no-cache, no-store, must-revalidate',
-    'Content-Type'                : 'application/javascript'
-  });
+var twit = new twitter({
+    consumer_key: process.argv[2],
+    consumer_secret: process.argv[3],
+    access_token_key: process.argv[4],
+    access_token_secret: process.argv[5]
+});
 
-  if (req.method === "GET") {
-    var params = url.parse(req.url, true).query;
+http.createServer(onRequest).listen(4242, "0.0.0.0");
 
-    if (!_.isUndefined(params.toggle) && (params.toggle === "fnord")) {
-      lastPoll = new Date();
-    } else {
-      respondStatus(res);
-    }
-  }
-}).listen(4242, "0.0.0.0");
 console.log('Server running at http://127.0.0.1:4242/');
 
-function respondStatus (res) {
-  var minutesSinceLastPoll = _.isUndefined(lastPoll) ?
-                                closeAfterMinutes+1 :
-                                lastPoll.getMinutesBetween(new Date());
 
-  if(minutesSinceLastPoll > closeAfterMinutes) {
+function onRequest ( req, res ) {
+  setHeaders(res);
+  processRequest(req, res);
+}
+
+function setHeaders ( res ) {
+  res.writeHead(200, {
+    'Access-Control-Allow-Origin'   : '*',
+    'Access-Control-Allow-Methods'  : 'POST, GET, OPTIONS, PUT, DELETE',
+    'Access-Control-Allow-Headers'  : '*',
+    'Cache-Control'                 : 'no-cache, no-store, must-revalidate',
+    'Content-Type'                  : 'application/javascript'
+  });
+}
+
+function processRequest ( req, res ) {
+  if ( req.method === "GET" ) {
+    doGet(req, res);
+  }
+}
+
+function doGet ( req, res ) {
+  var params = url.parse(req.url, true).query;
+
+  if ( !_.isUndefined(params.toggle) && (params.toggle === "fnord") ) {
+    lastPoll = new Date();
+  }
+
+  respondStatus(res);
+}
+
+function respondStatus ( res ) {
+  var minutesSinceLastPoll = lastPoll === null ?
+    closeAfterMinutes + 1 :
+    lastPoll.getMinutesBetween(new Date());
+
+  if ( minutesSinceLastPoll > closeAfterMinutes ) {
+    if ( lastState ) tweetFnordStatus( !lastState );
+
+    lastState = false;
     res.end("false");
-  } else {
+  }
+  else {
+    if ( !lastState ) tweetFnordStatus( !lastState );
+
+    lastState = true;
     res.end("true");
   }
 }
+
+function tweetFnordStatus (status) {
+  if ( status ) {
+    tweet("#fNordeingang is now open! " + lastPoll);
+  }
+  else {
+    tweet("#fNordeingang has closed, see you soon! " + lastPoll);
+  }
+}
+
+function tweet (msg) {
+  twit
+    .updateStatus(msg, function (err, data) {});
+}
+
