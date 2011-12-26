53cadac279a51ab5dd6675098f7702684d36b123
Tristan Leo <mail@tristan-leo.de>
2011-12-26 12:50:28 +0100

    refactored client a little bit

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
 $(function () {
-  $.getJSON("http://status.fnordeingang.de", function ( status ) {
+  $.getJSON("http://status.fnordeingang.de", setFnordStatus);
+
+  function setFnordStatus ( status ) {
     if ( status ) {
       $("#status").html("Der fNordeingang ist zurzeit offen.");
     }
     else {
       $("#status").html("Der fNordeingang ist zurzeit geschlossen.");
     }
-  });
+  }
 });
